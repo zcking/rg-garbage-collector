8a262d553082948740eac1e1f25d4fa73f6a2093
Zachary King <zcking@pm.me>
2024-02-03 22:06:42 -0600

    refactor workspace structure

--- timer_rg_garbage_collector/function_app.py
+++ timer_rg_garbage_collector/function_app.py
@@ -10,16 +10,16 @@ subscription_id = os.environ["AZURE_SUBSCRIPTION_ID"]
 
 app = func.FunctionApp()
 
-@app.function_name(name="rg_garbage_collector")
-@app.schedule(schedule="0 0 0 * * *", arg_name="my_timer", run_on_startup=True,
-              use_monitor=False)
-def timer_trigger(my_timer: func.TimerRequest) -> None:
+@app.function_name(name="timer_rg_garbage_collector")
+# @app.schedule(schedule="0 0 0 * * *", arg_name="timer", run_on_startup=True,
+#               use_monitor=False)
+def main(timer: func.TimerRequest) -> None:
     """Timer trigger function for scanning Resource Groups for deletion.
 
     Args:
-        my_timer (func.TimerRequest): azure function timer request
+        timer (func.TimerRequest): azure function timer request
     """
-    if my_timer.past_due:
+    if timer.past_due:
         logging.info('The timer is past due!')
 
     logging.info('Python timer trigger function executed.')
