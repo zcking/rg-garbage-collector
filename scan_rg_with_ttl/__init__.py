3adf1772640835f0077e4c6206b665cc7c5bc6e6
Zachary King <zcking@pm.me>
2024-02-04 21:37:14 -0600

    feat: implement deletion based on tag DeleteAt

--- scan_rg_with_ttl/__init__.py
+++ scan_rg_with_ttl/__init__.py
@@ -1,6 +1,7 @@
 import logging
 import os
 import json
+from datetime import datetime, date
 
 from azure.identity import DefaultAzureCredential
 from azure.mgmt.resource import ResourceManagementClient
@@ -9,25 +10,24 @@ import azure.functions as func
 
 subscription_id = os.environ["AZURE_SUBSCRIPTION_ID"]
 
+TAG_DELETE_AT = "DeleteAt"
+
 app = func.FunctionApp()
 
-# @app.function_name(name="timer_rg_garbage_collector")
-# @app.schedule(schedule="0 0 0 * * *", arg_name="timer", run_on_startup=True,
-#               use_monitor=True)
-# def main(timer: func.TimerRequest) -> None:
-
-# @app.function_name(name="scan_rg_with_ttl")
 
 def main(req: func.HttpRequest) -> func.HttpResponse:
     """Timer trigger function for scanning Resource Groups for deletion.
 
+    The following features are implemented:
+    - Tag "DeleteAt" is used to give a date when the resource group should be deleted
+
+    TODO: refactor to use a timer trigger instead of HTTP trigger
     Args:
         timer (func.TimerRequest): azure function timer request
     """
     print("Function starting...")
-    logging.info(f'Request recieved: {req.method} {req.url}')
+    logging.info('Request received: %s %s', req.method, req.url)
 
-    logging.info('Python timer trigger function executed.')
     # Create a credential object using the default Azure credentials
     credential = DefaultAzureCredential()
 
@@ -35,18 +35,36 @@ def main(req: func.HttpRequest) -> func.HttpResponse:
     resource_client = ResourceManagementClient(credential, subscription_id)
 
     # Get all resource groups in the current subscription
-    resource_groups = resource_client.resource_groups.list()
+    # that have the "DeleteAt" tag populated
+    resource_groups = resource_client.resource_groups.list(filter="tagname eq 'DeleteAt'")
 
-    # Iterate over the resource groups and print their names
-    ttl_resource_groups = []
+    # Iterate through the resource groups and flag those that should be deleted
+    flagged_resource_groups = {}
     for resource_group in resource_groups:
-        # print(resource_group.name)
-        # print(resource_group.tags)
-        # print('---' * 10)
-        if resource_group.tags and "ttl" in resource_group.tags:
-            ttl_resource_groups.append(resource_group.id)
+        #sanity
+        if not resource_group.tags:
+            continue
+
+        # If the resource group is already being deleted, skip it
+        if resource_group.properties.provisioning_state == "Deleting":
+            continue
+
+        if TAG_DELETE_AT in resource_group.tags:
+            # Parse the date from the tag value, in the format "YYYY-MM-DD"
+            delete_at = datetime.strptime(resource_group.tags[TAG_DELETE_AT], "%Y-%m-%d")
+            if delete_at.date() <= date.today():
+                flagged_resource_groups[resource_group.name] = resource_group.as_dict()
+        
+    # Delete the flagged resource groups
+    for rg in flagged_resource_groups:
+        # Delete the resource group, or log an error if it fails
+        try:
+            logging.info(f"Deleting resource group {rg}...")
+            resource_client.resource_groups.begin_delete(rg)
+        except Exception as e:
+            logging.error(f"Failed to delete resource group {rg}: {e}")
 
     response_body = json.dumps({
-        "discovered": ttl_resource_groups,
+        "deleted": flagged_resource_groups,
     })
     return func.HttpResponse(response_body, mimetype="application/json")
